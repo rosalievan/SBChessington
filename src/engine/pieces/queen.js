ff5eef6e016977b8a6ceaaee4e931f482de19d4b
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-16 15:07:33 +0100

    queenmovement passing tests

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,7 @@
+import Player from '../player';
+import Square from '../square';
 import Piece from './piece';
+import GameSettings from '../../engine/gameSettings';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +9,34 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let output = []
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i != location.row){
+            output.push(
+                Square.at(i, location.col)
+            )}
+
+            if (i != location.col){
+            output.push(
+                Square.at(location.row, i)
+            )
+            }
+
+            if((location.row +i < 8) && (location.col - i >=0) && (i != 0)){
+                output.push(Square.at(location.row + i, location.col - i));
+            }
+            if((location.row - i >= 0) && (location.col + i < 8) && (i != 0)){ 
+                output.push(Square.at(location.row - i, location.col + i));
+            }
+            if((location.row - i >= 0) && (location.col - i >=0) && (i != 0)){
+                output.push(Square.at(location.row - i, location.col - i));
+            }
+            if((location.row + i <8 ) && (location.col + i < 8) && (i != 0)){
+                output.push(Square.at(location.row + i, location.col + i));
+            }
+        }
+        return output;
     }
 }
