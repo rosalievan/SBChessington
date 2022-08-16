0591e5bd44f9bd6adc268caa296df5c2b0328b26
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-16 12:23:34 +0100

    solving red-1

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import GameSettings from '../../engine/gameSettings';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -9,10 +10,28 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+        console.log(location)
+        
+        // white pawns
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+            for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+                if (location.row == 1){
+                    return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                } else {
+                    return [Square.at(location.row + 1, location.col)]
+                }
+            }
+        } 
+
+        // black bawns
+        else {
+            for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+                if (location.row == 6){
+                    return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+                } else {
+                    return [Square.at(location.row - 1, location.col)]
+                }
+            }
         }
     }
 }
