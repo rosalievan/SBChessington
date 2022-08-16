4835aca4c911d6ebe36cc059bedc04d6161e0783
Noorulain Fahad <Noorulain.Fahad@softwire.com>
2022-08-16 14:54:19 +0100

    BishopTestPassed

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,34 @@
+import Player from '../player';
+import Square from '../square';
 import Piece from './piece';
+import GameSettings from '../../engine/gameSettings';
 
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+       getAvailableMoves(board) {
+            let location = board.findPiece(this);
+            let moves=[];
+
+            for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+                if((location.row +i < 8) && (location.col - i >=0)){
+                    moves.push(Square.at(location.row + i, location.col - i));
+                }
+                if((location.row - i >= 0) && (location.col + i < 8)){ 
+                    moves.push(Square.at(location.row - i, location.col + i));
+                }
+                if((location.row - i >= 0) && (location.col - i >=0)){
+                    moves.push(Square.at(location.row - i, location.col - i));
+                }
+                if((location.row + i <8 ) && (location.col + i < 8)){
+                    moves.push(Square.at(location.row + i, location.col + i));
+                }
+            }
+            
+            return moves;
+        } 
+    
     }
-}
+    
