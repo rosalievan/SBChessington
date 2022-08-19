15314fc09a22787baab4db02c88f95333da0e9aa
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-19 13:49:18 +0100

    put parameters in object

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -14,44 +14,45 @@ export default class Pawn extends Piece {
         let row=location.row;
         let col=location.col;
         let output = new Set();
-        let size = GameSettings.BOARD_SIZE
 
         // 'can move one or two squares up on their first move'
         // pawnoptions structure: [startcolumn, moveforward, player, finalrow ]
         // interfacing -> define an interface (only in typescript)
 
-        // let pawnoptions = {
-        //     white : {
-        //         startcolumn : 1,
-        //         moveforward : 1,
-        //         playername : Player.White,
-        //         finalrow : 7
-        //     }
-        // }
+        let pawnoptions = {
+            white : {
+                startcolumn : 1,
+                moveforward : 1,
+                finalrow : 7
+            },
+            black : {
+                startcolumn: 6,
+                moveforward: -1,
+                finalrow: 0
+            }
+        }
 
-        // or create a function that takes in the relvant options, nested in an if statement that checks what player it is and passes through those parameters
+        let parameters
+        if (this.player == Player.BLACK){
+            parameters = pawnoptions.black
+        } else { 
+            parameters = pawnoptions.white}
 
+ 
+
+        let newrow = row + parameters.moveforward
+        let suggestedsquare1 = Square.at(newrow,  col)
+        let suggestedsquare2 = Square.at(newrow + parameters.moveforward, col)
         
-        let pawnoptions =  [[1, 1, Player.WHITE, 7], [6, -1, Player.BLACK, 0]]
-        console.log(row)
-        for (let i in pawnoptions){
-            let k = pawnoptions[i]
+        if (row == parameters.startcolumn && board.checkIfEmpty(suggestedsquare1) && board.checkIfEmpty(suggestedsquare2)){ 
+            output.add(suggestedsquare1)
+            output.add(suggestedsquare2);} 
 
-            if (this.player == k[2]) { 
-
-                let newrow = row + k[1]
-                let suggestedsquare1 = Square.at(newrow,  col)
-                let suggestedsquare2 = Square.at(newrow + k[1], col)
-                
-                if (row == k[0] && board.checkIfEmpty(suggestedsquare1) && board.checkIfEmpty(suggestedsquare2)){ 
-                    output.add(suggestedsquare1)
-                    output.add(suggestedsquare2);} 
-
-                else if ( board.checkIfEmpty(suggestedsquare1)){
-                    output.add(suggestedsquare1);
-                    }
+        else if ( board.checkIfEmpty(suggestedsquare1)){
+            output.add(suggestedsquare1);
+            }
                     
-        }}
+        
 
         // 'can move diagonally if there is a piece to take'
 
@@ -63,7 +64,6 @@ export default class Pawn extends Piece {
                 for (let j in pawnoptions2[i][0]){
                     let suggestedsquare = Square.at(row + pawnoptions2[i][1] , col + pawnoptions2[i][0][j])
 
-                    // would be nice to rewrite checkIfEmpty to be more succinct, now its a repetition of the coordinates
                     if (!(board.checkIfEmpty(suggestedsquare)) && board.getPiece(suggestedsquare).player != pawnoptions2[i][2]) {
                         output.add(suggestedsquare)
                     }
