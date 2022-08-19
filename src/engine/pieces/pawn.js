b52a263bccef676b343f49ab6bcdd3e39249d755
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-19 10:54:35 +0100

    generalized diagonal move code

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -46,16 +46,27 @@ export default class Pawn extends Piece {
                     
         }}}}
 
-        if (this.player == Player.WHITE){
-            let suggestedsquare = Square.at(row+1, col + 1)
-            if (!(board.checkIfEmpty(row + 1, col + 1)) && board.getPiece(suggestedsquare).player != Player.WHITE) {
-                output.push(suggestedsquare)
+        // 'can move diagonally if there is a piece to take'
+
+        let pawnoptions2 =  [ [[1, -1], 1, Player.WHITE], [[1, -1], -1, Player.BLACK]]
+
+        for (let i in pawnoptions2){
+            if (this.player == pawnoptions2[i][2]){
+
+                for (let j in pawnoptions2[i][0]){
+                    let suggestedsquare = Square.at(row + pawnoptions2[i][1] , col + pawnoptions2[i][0][j])
+                    // would be nice to rewrite checkIfEmpty to be more succinct, now its a repetition of the coordinates
+                    if (!(board.checkIfEmpty(row + pawnoptions2[i][1] , col + pawnoptions2[i][0][j])) && board.getPiece(suggestedsquare).player != pawnoptions2[i][2]) {
+                        
+                        output.push(suggestedsquare)
+                    }
+                }
             }
 
-            let suggestedsquare2 = Square.at(row+1, col - 1)
-            if (!(board.checkIfEmpty(row + 1, col - 1)) && board.getPiece(suggestedsquare2).player != Player.WHITE) {
-                output.push(suggestedsquare2)
-            }
+            // let suggestedsquare2 = Square.at(row+1, col - 1)
+            // if (!(board.checkIfEmpty(row + 1, col - 1)) && board.getPiece(suggestedsquare2).player != Player.WHITE) {
+            //     output.push(suggestedsquare2)
+            // }
         }
 
         return output
