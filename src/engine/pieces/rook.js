6fd5effbe64f99a41ffb9db9ffd40311f17f5cb9
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-26 09:46:24 +0100

    improved code pending refactor

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -12,28 +12,59 @@ export default class Rook extends Piece {
         let location = board.findPiece(this);
         let row=location.row;
         let col=location.col;
-        let output = [];
+        let output = new Set();
         
-
-
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            if (i!= row){
-                let suggestedsquare1 = Square.at(i, col)
-                if(board.checkIfEmpty(suggestedsquare1)){
-                     output.push(suggestedsquare1);
-                }
-                else{
-                break}
+        for (let i = 1; i <= GameSettings.BOARD_SIZE; i++) {
+                let suggestedsquare1 = Square.at(row + i, col)
+                if(suggestedsquare1.isPossibleSquare()){
+                    if(board.checkIfEmpty(suggestedsquare1)){
+                        output.add(suggestedsquare1);
+                    }
+                    else {
+                        break
+                    }
             }
+        }
+        for (let i = 1; i <= GameSettings.BOARD_SIZE; i++) {
+                let suggestedsquare2 = Square.at(row - i, col)
+                if(suggestedsquare2.isPossibleSquare()){
+                    if(board.checkIfEmpty(suggestedsquare2)){
+                        output.add(suggestedsquare2);
+                    }
+                    else {
+                        break
+                    }
+            }
+        }
+        for (let i = 1; i <= GameSettings.BOARD_SIZE; i++) {
+                let suggestedsquare3 = Square.at(row, col + i)
+                if (suggestedsquare3.isPossibleSquare()){
+                    if(board.checkIfEmpty(suggestedsquare3)){
+                        output.add(suggestedsquare3);
+                    }
 
-            if (i!= col){
-                let suggestedsquare2 = Square.at(row, i)
-                if(board.checkIfEmpty(suggestedsquare2)){
-                    output.push(suggestedsquare2);
-                } else {
-                    break
-                }}}
-            return output;  
+                    else {
+                        break
+                    }
+            }
+        }
+
+        for (let i = 1; i <= GameSettings.BOARD_SIZE; i++) {
+
+                let suggestedsquare4 = Square.at(row, col - i)
+                if (suggestedsquare4.isPossibleSquare()){
+                    if(board.checkIfEmpty(suggestedsquare4)){
+                        output.add(suggestedsquare4);
+                    }
+
+                    else {
+                        break
+                    }
+            }
+        }
+    
+        output = Array.from(output)
+        return output  
     
     }
 }
